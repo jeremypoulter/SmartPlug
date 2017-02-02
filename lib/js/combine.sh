a9d5e01f0bb5dc4b8332c8f3e723f075b70d92f9
Jeremy Poulter <jeremy@bigjungle.net>
2017-02-02 23:03:29 +0000

    Smaller UI that fits in the 128K of the flash

--- lib/js/combine.sh
+++ lib/js/combine.sh
@@ -3,12 +3,30 @@
 LIB_DIR=$(dirname $(realpath $0))
 SRC_DIR=$LIB_DIR/../../src/data
 
-cat $LIB_DIR/jquery.min.js \
+rm $SRC_DIR/lib.*
+
+C=1
+
+if [ 1 -eq $C ]; then
+  COMP="gzip -c"
+  OUT=.gz
+else
+  COMP="cat"
+  OUT=
+fi
+
+#cat $LIB_DIR/jquery.min.js \
+#    $LIB_DIR/knockout.min.js \
+#    $LIB_DIR/knockout.mapping.min.js \
+#    $LIB_DIR/sammy.min.js \
+#    $LIB_DIR/bootstrap.min.js \
+#    | $COMP > $SRC_DIR/lib.js$OUT
+
+#cat $LIB_DIR/bootstrap.min.css \
+#    | $COMP > $SRC_DIR/lib.css$OUT
+
+cat $LIB_DIR/jquery.slim.min.js \
     $LIB_DIR/knockout.min.js \
     $LIB_DIR/knockout.mapping.min.js \
     $LIB_DIR/sammy.min.js \
-    $LIB_DIR/bootstrap.min.js \
-    > $SRC_DIR/lib.js
-
-cat $LIB_DIR/bootstrap.min.css \
-    > $SRC_DIR/lib.css
+    | $COMP > $SRC_DIR/lib.js$OUT
